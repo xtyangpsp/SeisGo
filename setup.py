376967dae7db09dfe8649dcfcd53e8e949bcb6ba
Xiaotao Yang <stcyang@gmail.com>
2021-06-01 11:52:43 -0400

    fix bugs

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from numpy.distutils.core import setup
 
 setup(
     name='seispy',
-    version='0.4.1',
+    version='0.5.0',
     description='A Python package for seismic data analysis',
     author='Xiaotao Yang',
     author_email='stcyang@gmail.com',
     maintainer='Xiaotao Yang',
     maintainer_email='stcyang@gmail.com',
-    download_url='https://github.com/xtyangpsp/SeisPy/archive/refs/tags/v0.4.1.tar.gz',
+    download_url='https://github.com/xtyangpsp/SeisPy/archive/refs/tags/v0.5.0.tar.gz',
     classifiers=[
-        'Development Status :: Beta',
-        'License :: MIT License',
-        'Programming Language :: Python :: >=3.6'],
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'],
     install_requires=['numpy',
                         'scipy',
                         'pandas',
