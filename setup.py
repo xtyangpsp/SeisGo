5ee141bf6d825fcbfbe02cd1222d3dfe59aa4258
Xiaotao Yang <stcyang@gmail.com>
2022-08-25 13:19:57 -0400

    limit number version for stackwell

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy',
+    install_requires=['numpy<=1.22.4',
                         'scipy',
                         'pandas',
                         'obspy',
