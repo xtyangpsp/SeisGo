2dec1e162df0747128f9e25e552ebe7ff3a0b76b
Xiaotao Yang <stcyang@gmail.com>
2023-09-29 16:28:42 -0400

    change numpy requirement to accommodate pyasdf

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy<=1.22.4',
+    install_requires=['numpy<1.26.0',
                         'scipy',
                         'pandas',
                         'obspy',
